d92fe2041d302f2a78e7d9625b1751c35a176bc2
nigthblue <user@>
2019-03-11 23:01:57 +0300

    task_love-triangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var c=0;
+  for (i = 1; i <= preferences.length; i++) {
+	if((preferences[i-1]==0)||((preferences[(preferences[i-1])-1])==0)||(preferences[i-1]==preferences[(preferences[i-1])-1])){
+		continue;
+	}
+	if((true)&&i==(preferences[(preferences[(preferences[i-1])-1])-1])){
+		c++;
+	}
+	}
+  return c/3;
 };
